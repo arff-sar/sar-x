5a7ef46b8425414fe56ec1557623866aff0ff756
arff-sar <arff.sar@gmail.com>
2026-03-27 13:25:00 +0300

    Add alembic merge migration for multiple heads

---
+++ migrations/versions/cfd5e1cc9bd2_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: cfd5e1cc9bd2
+Revises: 0b7c6a1d2e3f, f1d2e3c4b5a6
+Create Date: 2026-03-27 13:24:29.565439
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cfd5e1cc9bd2'
+down_revision = ('0b7c6a1d2e3f', 'f1d2e3c4b5a6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
